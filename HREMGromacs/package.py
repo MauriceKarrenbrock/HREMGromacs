c3da75b50a70381a8a4017b8a0f77626b18d8a1a
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2021-05-18 10:31:31 +0200

    setup basic stuff

---
+++ HREMGromacs/package.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Template package file"""
+__title__ = 'HREMGromacs'
+__version__ = '0.1'
+__author__ = 'Maurice Karrenbrock'
+__summary__ = ''
+__url__ = 'https://github.com/MauriceKarrenbrock/HREMGromacs'
+__email__ = ''
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2021, Maurice Karrenbrock'
+__classifiers__ = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+]
